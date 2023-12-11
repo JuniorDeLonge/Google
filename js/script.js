79c29b5f0de7095a96789b32ae7c3e692048fb64
Junior DeLonge <147082536+JuniorDeLonge@users.noreply.github.com>
2023-12-11 01:04:11 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,45 +1,78 @@
-function searchGoogle() {
-    const searchInput = document.getElementById("searchInput");
-    const searchResults = document.getElementById("searchResults");
+document.addEventListener("DOMContentLoaded", function () {
+  // @juniordelonge
+  var searchInput = document.querySelector(".search_camp input");
+  var closerButton = document.getElementById("closer");
+  var searchButton = document.querySelector(".search_btn button");
+  var acrAddElement = document.querySelector(".ACRAdd.M2vV3");
 
-    const query = searchInput.value;
+  var search_text = "";
 
-    if (query.trim() === '') {
-        alert('Por favor, insira um termo de pesquisa.');
-        return false;
+  // Oculte o elemento ACRAdd por padrão
+  acrAddElement.style.display = "none";
+
+  // Adicione um ouvinte de evento de entrada ao campo de pesquisa
+  searchInput.addEventListener("input", function () {
+    // Adicione a classe 'show' ao botão quando o usuário começar a digitar
+    closerButton.classList.add("show");
+
+    // Atualize a variável de texto de pesquisa
+    search_text = searchInput.value;
+
+    // Verifique se o campo de pesquisa está vazio
+    if (search_text === "") {
+      // Se estiver vazio, remova a classe 'show' para ocultar o botão
+      closerButton.classList.remove("show");
+
+      // Oculte o elemento ACRAdd
+      acrAddElement.style.display = "none";
+    } else {
+      // Se houver texto, adicione a classe 'show' ao botão
+      closerButton.classList.add("show");
+
+      // Mostre o elemento ACRAdd
+      acrAddElement.style.display = "block";
     }
+  });
+  // @juniordelonge
 
-    const cx = 'a31b17ca840904da5';
+  // Adicione um ouvinte de evento de clique ao botão "closer"
+  closerButton.addEventListener("click", function () {
+    // Limpe o valor do campo de pesquisa
+    searchInput.value = "";
 
-    const apiUrl = `https://www.googleapis.com/customsearch/v1?key=AIzaSyD5FBuMk_7_CCk4ws1qE4Z5uNZaTG7CACY&q=${query}&cx=${cx}`;
+    // Remova a classe 'show' para ocultar o botão
+    closerButton.classList.remove("show");
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            displaySearchResults(data.items);
-            searchResults.style.display = 'flex';
-        })
-        .catch(error => {
-            console.error('Erro ao realizar a busca:', error);
-        });
+    // Oculte o elemento ACRAdd
+    acrAddElement.style.display = "none";
+  });
 
-    return false;
-}
+  // Adicione um ouvinte de evento de clique ao botão de pesquisa
+  searchButton.addEventListener("click", function () {
+    googleSearch();
+  });
+  // @juniordelonge
 
-function displaySearchResults(results) {
-    const searchResults = document.getElementById("searchResults");
-    searchResults.innerHTML = '';
+  // Adicione um ouvinte de evento de tecla ao campo de pesquisa
+  searchInput.addEventListener("keyup", function (e) {
+    // Atualize a variável de texto de pesquisa
+    search_text = searchInput.value;
 
-    if (!results || results.length === 0) {
-        searchResults.innerHTML = '<p>Nenhum resultado encontrado.</p>';
-        return;
+    // Verifique se a tecla pressionada é "Enter"
+    if (e.key === "Enter") {
+      googleSearch();
     }
+  });
 
-    results.forEach(result => {
-        const resultItem = document.createElement('div');
-        resultItem.style.textAlign = 'left';
-        resultItem.innerHTML = `<h3><a href="${result.link}" target="_blank">${result.title}</a></h3><p>${result.snippet}</p>`;
-        searchResults.appendChild(resultItem);
-    });
+  function googleSearch() {
+    // Verifique se há texto de pesquisa
+    if (search_text.length >= 1) {
+      // Construa a URL de pesquisa do Google
+      var url = "https://google.com/search?q=" + search_text;
 
-}
+      // Redirecione para a URL de pesquisa do Google
+      window.location.href = url;
+    }
+  }
+});
+// @juniordelonge
